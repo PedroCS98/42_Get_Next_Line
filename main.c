55b9c3ddd872c8de159a4131e8fdc5bb5f090627
PEDRO SIMOES <pedrosimoes98@hotmail.com>
2024-04-23 22:48:18 +0100

    Created a test main file that is trying to read a test.txt file that contains a poem from Camoes

---
+++ main.c
@@ -0,0 +1,41 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = -1;
+	while (s[++i])
+		;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	if(!s)
+		return (NULL);
+	i = -1;
+	while (++i <= (int)ft_strlen(s))
+		if (s[i] == (char) c)
+			return ((char *)(s + i));
+	return (NULL);
+}
+
+int	main()
+{
+	char buffer[BUFFER_SIZE];
+	int fd  = open("test.txt", O_RDONLY);
+	int x = read(fd, buffer, BUFFER_SIZE);
+	if (ft_strchr(buffer, '\n'))
+	{
+		printf("found newline\n");
+		return (0);
+	}
+	printf("String %s has %d chars.\n", buffer, x);
+	close(fd);
+
+}
