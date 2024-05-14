78bdb48f335df63a524b19c0a38b94aa19fc4b89
PEDRO SIMOES <pedrosimoes98@hotmail.com>
2024-05-15 00:00:27 +0100

    put new corrections for analysis and changed slightly main so it doesnt repeat code

--- main.c
+++ main.c
@@ -4,38 +4,65 @@
 
 int	main()
 {
-	int i;
+	char	*line;
+	int 	i;
 
 	int fd  = open("test.txt", O_RDONLY);
 	i = 0;
-	//while (get_next_line(fd) < )
-	//{
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-		printf("%s\n", get_next_line(fd));
-
-	//}
+	while ((line = get_next_line(fd)) && ++i)
+		printf(/*line %d - */"%s", /*i,*/ line);
 	close(fd);
 	return (0);
 }
+
+/*
+
+Nao posso ter a verificacao inicial do get net line com o ffersize a 0
+
+A good start would be to know what a static variable is
+
+get_next_line() function should let you read the text file 
+pointed to by the file descriptor, one line at a time
+
+our function should return the line that was read.
+If there is nothing else to read or if an error occurred, it should return NULL.
+
+Make sure that your function works as expected both when reading a file and when
+reading from the standard input.------------------
+
+Please note that the returned line should include the terminating \n character,
+except if the end of file was reached and does not end with a \n character.
+
+Because you will have to read files in get_next_line(), add this option to your
+compiler call: -D BUFFER_SIZE=n----------
+It will define the buffer size for read().
+The buffer size value will be modified by your peer-evaluators and the Moulinette
+in order to test your code.-------------
+
+You will compile your code as follows (a buffer size of 42 is used as an example):
+cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 <files>.c
+
+We consider that get_next_line() has an undefined behavior if the file pointed to
+by the file descriptor changed since the last call whereas read() didn’t reach the
+end of file.---------------
+
+We also consider that get_next_line() has an undefined behavior when reading
+a binary file. However, you can implement a logical way to handle this behavior if
+you want to.------------
+
+BONUS---------------
+
+Develop get_next_line() using only one static variable.
+
+Your get_next_line() can manage multiple file descriptors at the same time.
+For example, if you can read from the file descriptors 3, 4 and 5, you should be
+able to read from a different fd per call without losing the reading thread of each
+file descriptor or returning a line from another fd.
+
+Append the _bonus.[c\h] suffix to the bonus part files.
+It means that, in addition to the mandatory part files, you will turn in the 3 following
+files:
+• get_next_line_bonus.c
+• get_next_line_bonus.h
+• get_next_line_utils_bonus.c
+*/
