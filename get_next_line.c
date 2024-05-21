d418757cce8fa3a95e4c17dc33ffef42d49598bb
PEDRO SIMOES <pedrosimoes98@hotmail.com>
2024-05-21 22:45:30 +0100

    norminette corrections; removed debugging some comments; restructered some of the code to have a more proper behavior

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimoes <psimoes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 20:27:31 by psimoes           #+#    #+#             */
+/*   Updated: 2024/05/21 22:38:00 by psimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
+char	*clean_stash(char *old_stash, char *line_read)
+{
+	char	*new_stash;
+	int		i;
+	int		j;
+
+	new_stash = (char *)malloc(ft_strlen(old_stash) - ft_strlen(line_read) + 1);
+	if (new_stash == NULL)
+		return (NULL);
+	i = ft_strlen(line_read) - 1;
+	j = 0;
+	while (/*++i <= ft_strlen(old_stash)*/ /*&& old_stash[i] != '\n' &&*/ old_stash[++i] != '\0')
+		new_stash[j++] = old_stash[i];
+	new_stash[i] = '\0';
+	free(old_stash);
+	return (new_stash);
+}
 
 char	*put_stash_in_line(char *stash)
 {
-	// receive stash
-	// count char until \n or \0 and malloc that size
-	// copy stash up to \n or \0 to line
-	// return line
 	char	*line_2_read;
 	int		i;
 
@@ -24,61 +49,38 @@ char	*put_stash_in_line(char *stash)
 		line_2_read[i] = stash[i];
 	line_2_read[++i] = '\0';
 	//printf("\nThe stash was: %s\nthe line returned is: %s\n", stash, line_2_read);
+	//stash = clean_stash(stash, line_2_read);
 	return (line_2_read);
 }
 
-char	*clean_stash(char *old_stash, char *line_read)
-{
-	// receice old_stash
-	// create new_stash 
-	// copy old_stash after \n to new_stash
-	// free old_stash
-	// return new_stash
-	char	*new_stash;
-	int		i;
-	int		j;
-
-	new_stash = (char *)malloc(ft_strlen(old_stash) - ft_strlen(line_read) + 1);
-	if (new_stash == NULL)
-		return (NULL);
-	i = ft_strlen(line_read) - 1;
-	j = 0;
-	while (/*++i <= ft_strlen(old_stash)*/ /*&& old_stash[i] != '\n' &&*/ old_stash[++i] != '\0')
-		new_stash[j++] = old_stash[i];
-	new_stash[i] = '\0';
-	free(old_stash);
-	//free(line_read);
-	return (new_stash);
-}
-
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char		buffer[BUFFER_SIZE + 1];
 	char		*line_2_read;
 	ssize_t		bytes_read;
 
-	bytes_read = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) == -1)
+	bytes_read = 1;
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	//bytes_read = 0;
 	if (!ft_strchr(stash, '\n') /*|| !ft_strchr(buffer, '\0')*/)
-		while ((bytes_read = read(fd, buffer, BUFFER_SIZE)))
+	{
+		while (bytes_read > 0)
 		{
-			//printf("\nThe buffer is: %s\n", buffer);
+			bytes_read = read(fd, buffer, BUFFER_SIZE);
 			stash = ft_strjoin(stash, buffer);
-			if(ft_strchr(buffer, '\n') /*|| ft_strchr(buffer, '\0')*/)
-					break;
+			if (ft_strchr(buffer, '\n') /*|| ft_strchr(buffer, '\0')*/)
+				break ;
 		}
+	}
 	//printf("\nThe stash is - %s\n\n", stash);
 	//printf("%zu bytes - ", bytes_read);
-	if (!bytes_read)
+	if (bytes_read <= 0)
 	{
 		free(stash);
 		return (NULL);
 	}
-	line_2_read = put_stash_in_line(stash); // creates line that is \n terminated 
-
-	stash = clean_stash(stash, line_2_read); // empties stash leaving only the beginning of next line
+	line_2_read = put_stash_in_line(stash);
+	stash = clean_stash(stash, line_2_read);
 	return (line_2_read);
 }
