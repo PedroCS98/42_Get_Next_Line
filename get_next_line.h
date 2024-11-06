be625611fa17506cfc252e053bd43f1f167439c2
Pedro Simoes <pedrosimoes98@hotmail.com>
2024-11-06 18:20:58 +0000

    corrected function names and changed default buffer size

--- get_next_line.h
+++ get_next_line.h
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 20
+#  define BUFFER_SIZE 42
 # endif
 
 # include <unistd.h>
@@ -24,15 +24,10 @@
 # include <limits.h>
 
 char	*get_next_line(int fd);
-//char	*fetch_line(int fd, char *str, char *buffer, int bytes_read);
-char	*put_line_in_str(char *str);
-char	*set_next_line(char *old_str, char *line_read);
-
-size_t	ft_strlen(const char *s);
+char	*put_stash_in_line(char *stash);
+char	*clean_stash(char *old_stash, char *line_read);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char const *s2);
-//size_t	ft_strlcat(char *dst, const char *src, size_t size);
-//size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-//void	*ft_memset(void *s, int c, size_t n);
+size_t	ft_strlen(const char *s);
 
 #endif
